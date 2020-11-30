27b401496f914520a93d2a1bc46c692b27691a89
unknown <racheli@eazyvat.com>
2020-11-30 11:23:21 +0200

    commit layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutContainerComponent } from './layout-container/layout-container.component';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { BottomMenuComponent } from'../app/bottom-menu/bottom-menu.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutContainerComponent,
+    BottomMenuComponent,
+    TopMenuComponent,
+   
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-menu/top-menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-top-menu',
+  templateUrl: './top-menu.component.html',
+  styleUrls: ['./top-menu.component.scss']
+})
+export class TopMenuComponent implements OnInit {
+
+  openTopMenu:boolean;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+
+
+
+}
